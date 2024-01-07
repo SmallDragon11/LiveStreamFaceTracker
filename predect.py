1a5df925d701ec8f62be7d8afad7a8a5aaf83e9d
Small Dragon <mick20001108@gmail.com>
2024-01-07 20:18:59 +0800

    xunxun

---
+++ predect.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import time
+import torch
+
+from yolox.data.data_augment import ValTransform
+from yolox.data.datasets import COCO_CLASSES
+from yolox.exp import get_exp
+from yolox.utils import fuse_model, get_model_info, postprocess, vis
+
+
+
+class Predictor(object):
+    def __init__(
+        self,
+        model,
+        exp,
+        cls_names=COCO_CLASSES,
+        trt_file=None,
+        decoder=None,
+        device="cpu",
+        fp16=False,
+        legacy=False,
+    ):
+        self.model = model
+        self.cls_names = cls_names
+        self.decoder = decoder
+        self.num_classes = exp.num_classes
+        self.confthre = exp.test_conf
+        self.nmsthre = exp.nmsthre
+        self.test_size = exp.test_size
+        self.device = device
+        self.fp16 = fp16
+        self.preproc = ValTransform(legacy=legacy)
+        if trt_file is not None:
+            from torch2trt import TRTModule
+
+            model_trt = TRTModule()
+            model_trt.load_state_dict(torch.load(trt_file))
+
+            x = torch.ones(1, 3, exp.test_size[0], exp.test_size[1]).cuda()
+            self.model(x)
+            self.model = model_trt
+
+    def inference(self, img):
+        img_info = {"id": 0}
+
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+
+        ratio = min(self.test_size[0] / img.shape[0], self.test_size[1] / img.shape[1])
+        img_info["ratio"] = ratio
+
+        img, _ = self.preproc(img, None, self.test_size)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.float()
+        if self.device == "gpu":
+            img = img.cuda()
+            if self.fp16:
+                img = img.half()  # to FP16
+
+        with torch.no_grad():
+            t0 = time.time()
+            outputs = self.model(img)
+            if self.decoder is not None:
+                outputs = self.decoder(outputs, dtype=outputs.type())
+            outputs = postprocess(
+                outputs, self.num_classes, self.confthre,
+                self.nmsthre, class_agnostic=True
+            )
+            # logger.info("Infer time: {:.4f}s".format(time.time() - t0))
+        return outputs, img_info
+
+    def visual(self, output, img_info, cls_conf=0.35, cls_list = []):
+        ratio = img_info["ratio"]
+        img = img_info["raw_img"]
+        if output is None:
+            return img
+        output = output.cpu()
+
+        bboxes = output[:, 0:4]
+
+        # preprocessing: resize
+        bboxes /= ratio
+
+        cls = output[:, 6]
+        scores = output[:, 4] * output[:, 5]
+
+        # print('cls_conf:',cls_conf, file=sys.stderr)
+        vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names, cls_list)
+        return vis_res
+
+
+def image_demo(predictor, img, conf, cls):
+    
+    outputs, img_info = predictor.inference(img)
+    result_image = predictor.visual(outputs[0], img_info, conf, cls)
+
+    return result_image
+
---
+++ server.py
@@ -0,0 +1,324 @@
+from flask import Flask, Response, render_template, redirect, request, session
+import cv2, sys, os
+import argparse
+import os
+
+##########James############
+# from yolox.data.datasets import COCO_CLASSES
+# from yolox.exp import get_exp
+# from predect import Predictor, image_demo
+##########James############
+
+##########smallJames###########
+import numpy as np
+from yolox.tracker.byte_tracker import BYTETracker
+from yolox.utils.visualize import plot_tracking
+from yolox.tracking_utils.timer import Timer
+##########smallJames###########
+
+
+#Initialize the Flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+camera = cv2.VideoCapture(0)
+curent = None
+
+def gen_frames(res = '1080p', cls = [], conf = 0.6):
+
+    ##########James############
+    # exp = get_exp(None, 'yolox-s')
+    # model = exp.get_model()
+    # model.eval()
+
+    # ckpt_file = 'YOLOX/yolox_s.pth'
+    # ckpt = torch.load(ckpt_file, map_location="cpu")
+    # model.load_state_dict(ckpt["model"])
+    # predictor = Predictor( model, exp, COCO_CLASSES, None, None, 'cpu', False, False)
+    ##########James############
+
+    ##########smallJames###########
+    parser = argparse.ArgumentParser(description='Code for FaceTrackCam.')
+    parser.add_argument('--camera', '-cam', help='Camera divide number.', type=int, default=0)
+
+    # from bytetrack demo.py
+    # ==========================================
+    parser.add_argument("--fps", default=30, type=int, help="frame rate (fps)")
+
+    parser.add_argument("--track_thresh", type=float, default=0.5, help="tracking confidence threshold")
+    parser.add_argument("--track_buffer", type=int, default=30, help="the frames for keep lost tracks")
+    parser.add_argument("--match_thresh", type=float, default=0.8, help="matching threshold for tracking")
+    parser.add_argument(
+            "--aspect_ratio_thresh", type=float, default=1.6,
+            help="threshold for filtering out boxes of which aspect ratio are above the given value."
+        )
+    parser.add_argument('--min_box_area', type=float, default=10, help='filter out tiny boxes')
+    parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")
+    # ==========================================
+    args = parser.parse_args()
+
+    net = cv2.dnn.readNetFromDarknet('yolov4-tiny-obj.cfg', 'yolov4-tiny-obj_last.weights')
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX # FPS font
+    timer = Timer()
+
+    tracker = BYTETracker(args, frame_rate=args.fps)
+    frame_id = 0
+    ##########smallJames###########
+
+    while True:
+        success, frame = camera.read()  # read the camera frame
+       
+        if not success:
+            break
+        else:
+            # print(session['resolution'], file=sys.stderr)
+            if   res == '720p':
+                frame = cv2.resize(frame, (1280, 720))
+            elif res == '480p':
+                frame = cv2.resize(frame, (854, 480))
+            elif res == '360p':
+                frame = cv2.resize(frame, (640, 360))
+            elif res == '240p':
+                frame = cv2.resize(frame, (426, 240))
+
+            ##########smallJames###########
+            img_info = {"id": 0}
+            frame=cv2.flip(frame,1)  #mirror the image
+            height, width = frame.shape[:2]
+            img_info['raw_img'] = frame
+            img_info["height"] = height
+            img_info["width"] = width
+            
+            timer.tic()
+            classIds, scores, faces = model.detect(frame, confThreshold=0, nmsThreshold=0.4)
+            
+            if len(faces)!=0:
+                faces[:, 2] = faces[:, 0] + faces[:, 2]
+                faces[:, 3] = faces[:, 1] + faces[:, 3]
+
+                scores = scores.reshape((len(faces), 1))
+                faces = np.concatenate((faces, scores), axis=1)
+                online_targets = tracker.update(faces, [img_info['height'], img_info['width']], (416, 416))
+                online_tlwhs = []
+                online_ids = []
+                online_scores = []
+                for t in online_targets:
+                    tlwh = t.tlwh
+                    tid = t.track_id
+                    vertical = tlwh[2] / tlwh[3] > args.aspect_ratio_thresh
+                    if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
+                        online_tlwhs.append(tlwh)
+                        online_ids.append(tid)
+                        online_scores.append(t.score)
+
+                timer.toc()
+                frame = plot_tracking(
+                            img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1, fps=1. / timer.average_time
+                        )
+                
+            else:
+                timer.toc()
+                frame = frame
+
+            frame_id += 1
+            ##########smallJames###########
+
+            ##########James############
+            # frame = image_demo(predictor, frame, conf = conf, cls = cls)
+            ##########James############
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            global current
+            current =  (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            yield current        
+
+# Get video
+@app.route('/video_feed')
+def video_feed():
+    try:
+        session['resolution']
+    except:
+        session['resolution'] = '1080p'
+
+    try:
+        session['cls']
+    except:
+        session['cls'] = []
+    
+    try:
+        session['conf']
+    except:
+        session['conf'] = 0.6
+
+    current = gen_frames(res = session['resolution'], cls = session['cls'], conf = session['conf'])
+
+    return Response(current, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+#start/stop
+@app.route('/')
+def index():
+    previous_data = {"selected_item":"1080p"}
+    return render_template('index.html',previous_data=previous_data)
+
+@app.route('/start', methods=['GET', 'POST'])
+def start():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Start':
+            return redirect('/')
+        elif request.form['submit_button'] == 'Pause':
+            try:
+                previous_data = {"selected_item":session['resolution'] }  
+            except:
+                previous_data = {"selected_item":"1080p" }  
+            return render_template('freeze.html', previous_data=previous_data)
+
+
+@app.route('/freeze')
+def freeze():
+    return Response(current, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/resolution', methods=['GET', 'POST'])
+def resolution():
+    if request.method == 'POST':
+        if request.form['operator'] == '1080p':
+            session['resolution'] = '1080p'
+        elif request.form['operator'] == '720p':
+            session['resolution'] = '720p'
+        elif request.form['operator'] == '480p':
+            session['resolution'] = '480p'
+        elif request.form['operator'] == '360p':
+            session['resolution'] = '360p'
+        elif request.form['operator'] == '240p':
+            session['resolution'] = '240p'
+        
+        previous_data = {"selected_item":session['resolution'] }      
+        return render_template('index.html',previous_data=previous_data)
+
+
+@app.route('/detect', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    text = str(text.lower())
+
+    try:
+        session['cls']
+    except:
+        session['cls'] = []
+
+    c = session['cls']
+    if request.form['submit_button'] == 'Add':
+        print('Add:', text, file=sys.stderr)
+        if text not in session['cls']:
+            c.append(text)
+            session['cls'] = c
+        
+            
+    elif request.form['submit_button'] == 'Remove':
+        print('Reomve:', text, file=sys.stderr)
+        if text in session['cls']:
+            c.remove(text)
+            session['cls'] = c
+
+    elif request.form['submit_button'] == 'submit':
+        session['conf'] = float(text)
+        print("session['conf']:", session['conf'], file=sys.stderr)
+
+    print("session['cls']:", session['cls'], file=sys.stderr)
+    
+    
+    try:
+        previous_data = {"selected_item":session['resolution'] }  
+    except:
+        previous_data = {"selected_item":"1080p" }  
+    return render_template('index.html', previous_data=previous_data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',  port='8000', debug=True, threaded=True)
+
+
+'''
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "dining table",
+    "toilet",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+'''
